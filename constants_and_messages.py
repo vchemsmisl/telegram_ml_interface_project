5727f58bb3477cb48d3dcf17238a7916a0aeb445
vchemsmisl <issakov.daanila@mail.ru>
2024-12-25 23:42:25 +0300

    _

--- constants_and_messages.py
+++
@@ -1,7 +0,0 @@
-BOT_TOKEN = '7825368344:AAHrruIowdwIr0m_mwQi-OM0M_mZxtEXl-U'
-
-STARTING_MESSAGE_TRUE = '''
-Welcome to the Simple ML Interface bot!
-Let's begin
-'''
-STARTING_MESSAGE_FALSE = 'Please enter "/start" or "/help" command'
